defc7d638ae8472d5eeed7619926190008a1868b
mash <mashnote@rambler.ru>
2022-07-28 18:26:55 +0300

    update tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,14 @@
+import { genDiff } from '../src/index.js';
+
+if (genDiff('__fixtures__/file1.json', '__fixtures__/file2.json') !== `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`) {
+  throw new Error('Функция работает неверно!');
+}
+
+console.log('Все тесты пройдены!');
