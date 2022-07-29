4fdc64594341c8d03f901005cb5cf258a6887760
mash <mashnote@rambler.ru>
2022-07-29 16:04:30 +0300

    update tests, index

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,3 +12,15 @@ if (genDiff('__fixtures__/file1.json', '__fixtures__/file2.json') !== `{
 }
 
 console.log('Все тесты пройдены!');
+
+test('gendiff', () => {
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(`{
+    - follow: false
+      host: hexlet.io
+    - proxy: 123.234.53.22
+    - timeout: 50
+    + timeout: 20
+    + verbose: true
+  }`);
+  //expect(reverse('')).toEqual('');
+});
